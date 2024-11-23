64e136f85e85b01b998f190bc57837e7719d7bef
Mikhail Atoyan <mihail.atoyan@yandex.ru>
2024-11-24 01:44:19 +0300

    complete navigation tasks

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Михаил Атоян on 23.11.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
+
