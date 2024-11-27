d9b596cea29cbd8fe9637d370c597e21fd85209e
Mikhail Atoyan <mihail.atoyan@yandex.ru>
2024-11-27 06:38:02 +0300

    complete intro to code layout tasks

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,5 +9,82 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    private var profileImageView = UIImageView()
+    
+    private var nameLabel = UILabel()
+    private var usernameLabel = UILabel()
+    private var descriptionLabel = UILabel()
+    
+    private var logoutButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addProfileImageView()
+        
+        addNameLabel()
+        addUsernameLabel()
+        addDescriptionLabel()
+        
+        addLogoutButton()
+    }
+    
+    private func addProfileImageView(){
+        view.addSubview(profileImageView)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.image = UIImage(named: "avatar")
+        profileImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+    }
+    
+    private func addNameLabel(){
+        view.addSubview(nameLabel)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.textColor = .white
+        nameLabel.font = .systemFont(ofSize: .init(23), weight: .semibold)
+        nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor).isActive = true
+    }
+    
+    private func addUsernameLabel(){
+        view.addSubview(usernameLabel)
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        usernameLabel.text = "@ekaterina_nov"
+        usernameLabel.textColor = UIColor(red: 174/255.0, green: 175/255.0, blue: 180/255.0, alpha: 1.0)
+        usernameLabel.font = .systemFont(ofSize: .init(13))
+        usernameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        usernameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+    }
+    private func addDescriptionLabel(){
+        view.addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.text = "Hello, World!"
+        descriptionLabel.textColor = .white
+        descriptionLabel.font = .systemFont(ofSize: .init(13))
+        descriptionLabel.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 8).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: usernameLabel.leadingAnchor).isActive = true
+    }
+    
+    private func addLogoutButton(){
+        logoutButton = UIButton.systemButton(with: UIImage(named: "logout_button") ?? UIImage(systemName: "ipad.and.arrow.forward")!,
+                                             target: self,
+                                             action: #selector(self.didTapButton))
+        view.addSubview(logoutButton)
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.tintColor = UIColor(red: 245/255.0, green: 107/255.0, blue: 108/255.0, alpha: 1.0)
+        
+        logoutButton.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        logoutButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor).isActive = true
+        logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+    }
+    @objc
+    private func didTapButton() {
+
+    }
 }
+
 
